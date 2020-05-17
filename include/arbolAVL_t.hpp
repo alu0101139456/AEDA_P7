8a897fc6ef0750a456e66356213ba5dc3e705920
Ángel Julián Bolaño Campos <alu0101139456@ull.edu.es>
2020-05-17 11:02:17 +0100

    Clase arbolAVL_t creada con la estructura de los metodos

---
+++ include/arbolAVL_t.hpp
@@ -0,0 +1,42 @@
+#pragma once
+
+
+#include "arbolB_t.hpp"
+
+
+
+
+template <class T>
+class arbolAVL_t : public arbolB_t<T> {
+
+ private:
+    
+
+ public:
+
+  void Rotacion_II(nodoB_t<T>* &nodo);
+  void Rotacion_DD(nodoB_t<T>* &nodo);
+  void Rotacion_ID(nodoB_t<T>* &nodo);
+  void Rotacion_DI(nodoB_t<T>* &nodo);
+
+  void Insertar(T claveDada);
+
+
+  void InsertaBal(nodoB_t<T>* &nodo, nodoB_t<T>* nuevo, bool& crece);
+  void InsertReBalIzq(nodoB_t<T>* nodo);
+  void InsertReBalDer(nodoB_t<T>* nodo);
+
+  void Eliminar(T clavedada);
+  void EliminarRama(nodoB_t<T>* &nodo, T clave, bool& decrece);
+
+  void Sustituye(nodoB_t<T>* &eliminado, nodoB_t<T>* &sust, bool &decrece);
+
+  void EliminarReBalIzq(nodoB_t<T>* &nodo, bool& decrece);
+  void EliminarReBalDer(nodoB_t<T>* &nodo, bool& decrece);
+
+
+    
+
+
+
+};
--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,13 @@
 #include <iostream>
+#include "arbolAVL_t.hpp"
+#include "dni_t.hpp"
+
 
 int main() {
     std::cout << "Practica 7" << std::endl;
+    arbolAVL_t<dni_t> a;
+
+    
+    
+    a.
 }
