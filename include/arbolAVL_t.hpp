1fe43e4ee344f4785e54f09cd5e00e4373beed3e
Ángel Julián Bolaño Campos <alu0101139456@ull.edu.es>
2020-06-01 11:57:33 +0100

    Metodos cambiados a privados

--- include/arbolAVL_t.hpp
+++ include/arbolAVL_t.hpp
@@ -11,33 +11,23 @@ class arbolAVL_t : public arbolB_t<T> {
 
  public:
 
+  void Insertar(T claveDada);
+  inline nodoB_t<T>* Buscar(T dato) { BuscarRama( this->getRaiz(), dato); }
+  void Eliminar(T clavedada);
+
+ private:
+  nodoB_t<T>* BuscarRama( nodoB_t<T> *nodo, T dato);
+  void EliminarRama(nodoB_t<T>* &nodo, T dato, bool &decrece);
+  void Sustituye(nodoB_t<T>* &eliminado, nodoB_t<T>* &sust, bool &decrece);
+  void EliminarReBalIzq(nodoB_t<T>* &nodo, bool &decrece);
+  void EliminarReBalDer(nodoB_t<T>* &nodo, bool &decrece);
+  void InsertaBal(nodoB_t<T>* &nodo, nodoB_t<T>* nuevo, bool &crece);
+  void InsertReBalIzq(nodoB_t<T>* &nodo, bool &crece);
+  void InsertReBalDer(nodoB_t<T>* &nodo, bool &crece);
   void Rotacion_II(nodoB_t<T>* &nodo);
   void Rotacion_DD(nodoB_t<T>* &nodo);
   void Rotacion_ID(nodoB_t<T>* &nodo);
   void Rotacion_DI(nodoB_t<T>* &nodo);
-
-  void Insertar(T claveDada);
-
-
-  inline nodoB_t<T>* Buscar(T dato) { BuscarRama( this->getRaiz(), dato); }
-
-  void InsertaBal(nodoB_t<T>* &nodo, nodoB_t<T>* nuevo, bool &crece);
-  void InsertReBalIzq(nodoB_t<T>* &nodo, bool &crece);
-  void InsertReBalDer(nodoB_t<T>* &nodo, bool &crece);
-
-  void Eliminar(T clavedada);
-  void EliminarRama(nodoB_t<T>* &nodo, T dato, bool &decrece);
-
-
-  void Sustituye(nodoB_t<T>* &eliminado, nodoB_t<T>* &sust, bool &decrece);
-
-  void EliminarReBalIzq(nodoB_t<T>* &nodo, bool &decrece);
-  void EliminarReBalDer(nodoB_t<T>* &nodo, bool &decrece);
-
- private:
-  nodoB_t<T>* BuscarRama( nodoB_t<T> *nodo, T dato);
-    
-
 
 
 };
